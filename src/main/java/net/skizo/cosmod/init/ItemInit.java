7788b56b7108fc2ad278290a223687a47133d18e
atomarthur <arthur22rai@gmail.com>
2026-02-15 22:38:35 -0300

    rapier added

--- src/main/java/net/skizo/cosmod/init/ItemInit.java
+++ src/main/java/net/skizo/cosmod/init/ItemInit.java
@@ -7,16 +7,23 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import net.skizo.cosmod.COSMOD;
+import net.skizo.cosmod.items.draken.tools.StingOfSacrament;
 import net.skizo.cosmod.items.hayate.tools.Kusanagi;
 import net.skizo.cosmod.items.misc.SpellItem;
 
 public class ItemInit {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, COSMOD.MODID);
 
+    // Hayate
     public static final RegistryObject<Item> KUSANAGI = ITEMS.register("kusanagi",
             () -> new Kusanagi(Tiers.DIAMOND, 4, -2.4f, new Item.Properties()));
     public static final RegistryObject<Item> aaa = ITEMS.register("aaa",
             () -> new SpellItem(new Item.Properties()));
+
+    // Draken
+
+    public static final RegistryObject<Item> STING_OF_SACRAMENT = ITEMS.register("sting_of_sacrament",
+            () -> new StingOfSacrament(Tiers.DIAMOND, 2, -3.0f, new Item.Properties()));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
---
+++ src/main/java/net/skizo/cosmod/items/draken/tools/StingOfSacrament.java
@@ -0,0 +1,10 @@
+package net.skizo.cosmod.items.draken.tools;
+
+import net.minecraft.world.item.Tier;
+import yesman.epicfight.world.item.WeaponItem;
+
+public class StingOfSacrament extends WeaponItem {
+    public StingOfSacrament(Tier tier, int damageIn, float speedIn, Properties builder) {
+        super(tier, damageIn, speedIn, builder);
+    }
+}
